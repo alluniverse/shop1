5e19648c87cc56441bf242a190c2151265211b7d
AllUniverse <zmeevik97@gmail.com>
2022-01-19 14:25:27 +0200

    CSS3 динамическая вёрстка

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.http import HttpResponseNotFound
+import os
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
